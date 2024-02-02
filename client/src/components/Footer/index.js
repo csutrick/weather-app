7cb8a1e5f946842f052e05e9b1c07d80b4ec52f0
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2024-02-02 12:56:56 -0600

    Added app layout with pages, components, App.js setup

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <nav className="flex h-72 items-center justify-center bg-red-300">
+      <h2 className="text-3xl font-bold text-black">Footer</h2>
+    </nav>
+  );
+};
+
+export default Footer;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="mt-16 flex h-screen items-center justify-center bg-blue-300">
+      <h2 className="text-6xl font-bold text-white">Home Page</h2>
+    </section>
+  );
+};
+
+export default Home;
