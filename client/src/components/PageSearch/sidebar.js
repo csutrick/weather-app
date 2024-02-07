8707c99b2f9efd1410e98c9f4c6a60cba7679347
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2024-02-06 23:00:10 -0600

    Added weather API search ability

--- client/src/components/PageSearch/sidebar.js
+++ client/src/components/PageSearch/sidebar.js
@@ -1,13 +1,35 @@
 import React from "react";
 
+import { useLazyQuery } from "@apollo/client";
+import { QUERY_WEATHER_FORECAST } from "../../utils/queries";
+
 import Input from "./input";
 import SearchHistory from "./searchHistory";
 
-const Sidebar = () => {
+const Sidebar = ({ setSearchResults }) => {
+  const [getWeatherForecast, { loading, data, error }] = useLazyQuery(
+    QUERY_WEATHER_FORECAST,
+    {
+      onCompleted: (data) => {
+        console.log("Weather data received:", data.getWeatherForecast); // Log received data
+        setSearchResults(data.getWeatherForecast);
+      },
+      onError: (error) => {
+        console.error("Error fetching weather data:", error); // Log error
+      },
+    },
+  );
+
+  const handleSearch = async (searchTerm) => {
+    console.log("Searching weather for:", searchTerm);
+
+    getWeatherForecast({ variables: { city: searchTerm } });
+  };
+
   return (
     <div className="w-1/4 bg-green-300 p-4">
       {/* User input */}
-      <Input />
+      <Input handleSearch={handleSearch} />
       {/* User search history */}
       <SearchHistory />
     </div>
