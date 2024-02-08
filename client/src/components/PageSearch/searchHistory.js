b042c5fc523d4e1170d1892bafa44915e8740a3a
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2024-02-08 12:20:19 -0600

    Added delete button, added button press functionality

--- client/src/components/PageSearch/searchHistory.js
+++ client/src/components/PageSearch/searchHistory.js
@@ -1,11 +1,30 @@
 import React, { useEffect } from "react";
 
-const SearchHistory = ({ searchHistory }) => {
+import { BsFillTrashFill } from "react-icons/bs";
+
+const SearchHistory = ({
+  handleSearch,
+  searchHistory,
+  setSearchHistory,
+  setSearchTerm,
+}) => {
   // Runs every time searchHistory changes
   useEffect(() => {
     console.log(`Search History changed updating with:`, searchHistory);
     localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
   }, [searchHistory]);
+
+  const handleDeleteSearch = (deletedCity) => {
+    const updatedHistory = searchHistory.filter((city) => city !== deletedCity);
+    localStorage.setItem("searchHistory", JSON.stringify(updatedHistory));
+    setSearchHistory(updatedHistory);
+  };
+
+  const histoyItemClick = (city) => {
+    setSearchTerm(city);
+
+    handleSearch(city);
+  };
 
   return (
     <div className="flex flex-col items-center justify-start bg-blue-300">
@@ -16,9 +35,17 @@ const SearchHistory = ({ searchHistory }) => {
         {searchHistory.map((city, index) => (
           <div
             key={index}
-            className="w-full bg-red-300 text-center text-lg font-bold"
+            className="flex w-full flex-row flex-nowrap bg-red-300 p-1 text-lg font-bold"
           >
-            {city}
+            <h3
+              onClick={() => histoyItemClick(city)}
+              className="w-full pl-2 text-2xl"
+            >
+              {city}
+            </h3>
+            <button onClick={() => handleDeleteSearch(city)} className="px-2">
+              <BsFillTrashFill className="text-2xl text-black hover:text-gray-300" />
+            </button>
           </div>
         ))}
       </div>
