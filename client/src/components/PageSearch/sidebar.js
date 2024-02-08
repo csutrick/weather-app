f15f8f121edbd40ee09c1378ab4069a0335834ca
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2024-02-08 11:10:34 -0600

    Added basic search history, renders all user past searches from local storage

--- client/src/components/PageSearch/sidebar.js
+++ client/src/components/PageSearch/sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useLazyQuery } from "@apollo/client";
 import { QUERY_WEATHER_FORECAST } from "../../utils/queries";
@@ -7,12 +7,24 @@ import Input from "./input";
 import SearchHistory from "./searchHistory";
 
 const Sidebar = ({ setSearchResults }) => {
+  const [searchHistory, setSearchHistory] = useState(() => {
+    const savedSearches = localStorage.getItem("searchHistory");
+    return savedSearches ? JSON.parse(savedSearches) : [];
+  });
+
   const [getWeatherForecast, { loading, data, error }] = useLazyQuery(
     QUERY_WEATHER_FORECAST,
     {
       onCompleted: (data) => {
         console.log("Weather data received:", data.getWeatherForecast); // Log received data
         setSearchResults(data.getWeatherForecast);
+
+        if (data.getWeatherForecast && data.getWeatherForecast.city) {
+          setSearchHistory((prevHistory) => [
+            data.getWeatherForecast.city,
+            ...prevHistory,
+          ]);
+        }
       },
       onError: (error) => {
         console.error("Error fetching weather data:", error); // Log error
@@ -31,7 +43,7 @@ const Sidebar = ({ setSearchResults }) => {
       {/* User input */}
       <Input handleSearch={handleSearch} />
       {/* User search history */}
-      <SearchHistory />
+      <SearchHistory searchHistory={searchHistory} />
     </div>
   );
 };
